d397fe9040965a832b520647f44c7b767d9b6445
Carlos <109424074+CarlosDcomp@users.noreply.github.com>
2023-04-26 17:09:27 -0300

    wip

--- database/migrations/2023_04_26_200843_create_institutions_table.php
+++ database/migrations/2023_04_26_200843_create_institutions_table.php
@@ -1,10 +1,12 @@
 <?php
 
+// database/migrations/{timestamp}_create_institutions_table.php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateInstitutionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +15,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('institutions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 200);
+            $table->string('address', 200);
             $table->timestamps();
         });
     }
@@ -31,6 +30,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('institutions');
     }
 }
+
